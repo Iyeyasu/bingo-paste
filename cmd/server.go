58621d326816c4239c10909bafe186d38a520bb0
Ieyasu <ieyasu@protonmail.com>
2020-01-21 03:04:29 +0200

    Add favicon

--- cmd/server.go
+++ cmd/server.go
@@ -18,5 +18,11 @@ func main() {
 	var endPoint api.PasteEndPoint
 	endPoint.Handle("/api/v1/paste/")
 
+	http.HandleFunc("/favicon.ico", faviconHandler)
+
 	log.Fatal(http.ListenAndServe(":80", nil))
+}
+
+func faviconHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "web/img/favicon.ico")
 }
